8dd537373058e0f0c4549fd7bb77bbb990b9a8d2
Aseer KT <aseermiqdad@gmail.com>
2021-03-22 22:11:46 +0530

    initial setup frontend and backend

---
+++ server/ormconfig.js
@@ -0,0 +1,15 @@
+const __prod__ = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'proshop',
+  synchronize: false,
+  logging: !__prod__,
+  entities: ['dist/entities/**/*.js'],
+  migrations: ['dist/migrations/**/*.js'],
+  subscribers: ['dist/subscribers/**/*.js'],
+};
---
+++ server/src/server.ts
@@ -0,0 +1,50 @@
+import 'reflect-metadata';
+import 'colors';
+import 'dotenv/config';
+import express from 'express';
+// import helmet from 'helmet';
+import { createConnection } from 'typeorm';
+import { buildSchema } from 'type-graphql';
+import { graphqlHTTP } from 'express-graphql';
+import cors from 'cors';
+
+createConnection()
+  .then((conn) =>
+    console.log(`Connected to Database: ${conn.driver.database}`.yellow.bold)
+  )
+  .catch((err) => {
+    console.error(`Database Connection Failed: ${err.message}`.red);
+    process.exit(1);
+  });
+
+const app = express();
+
+// app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+app.use(
+  '/graphql',
+  graphqlHTTP(async (req, res) => ({
+    schema: await buildSchema({
+      resolvers: [__dirname + '/resolvers/**/*.js'],
+    }),
+    context: { req, res },
+    graphiql: true,
+  }))
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(
+    `GraphQL API is running on http://localhost:${PORT}/graphql`.blue.bold
+  )
+);
